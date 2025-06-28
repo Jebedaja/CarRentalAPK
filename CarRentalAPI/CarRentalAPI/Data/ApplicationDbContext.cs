1568c69b4a3cb467657475ac54863823dddd89a6
Jebedaja <77245826+Jebedaja@users.noreply.github.com>
2025-06-28 03:59:19 +0200

    Bloby

--- CarRentalAPI/CarRentalAPI/Data/ApplicationDbContext.cs
+++ CarRentalAPI/CarRentalAPI/Data/ApplicationDbContext.cs
@@ -39,18 +39,22 @@ namespace CarRental.API.Data
                 .OnDelete(DeleteBehavior.Restrict);
 
 
+
             // Dodanie początkowych danych (seed data) - przydatne do testowania
             modelBuilder.Entity<City>().HasData(
-                new City { Id = 1, Name = "Warszawa", ImageUrl = "https://carrentalapkstorage.blob.core.windows.net/images/warsaw.jpg" },
-                new City { Id = 2, Name = "Kraków", ImageUrl = "https://carrentalapkstorage.blob.core.windows.net/images/krakow.jpg" },
-                new City { Id = 3, Name = "Gdańsk", ImageUrl = "https://carrentalapkstorage.blob.core.windows.net/images/gdansk.jpg" }
+                new City { Id = 1, Name = "Warszawa", ImageUrl = "https://carrentalapkstorage.blob.core.windows.net/obrazki/warszawa.jpg" },
+                new City { Id = 2, Name = "Kraków", ImageUrl = "https://carrentalapkstorage.blob.core.windows.net/obrazki/krak%C3%B3w.jpg" },
+                new City { Id = 3, Name = "Gdańsk", ImageUrl = "https://carrentalapkstorage.blob.core.windows.net/obrazki/Gdansk.jpg" },
+                new City { Id = 4, Name = "Mediolan", ImageUrl = "https://carrentalapkstorage.blob.core.windows.net/obrazki/Mediolan.jpg" }
             );
 
             modelBuilder.Entity<Car>().HasData(
-                new Car { Id = 1, CityId = 1, Brand = "Toyota", Model = "Corolla", Year = 2022, PricePerDay = 150.00m, ImageUrl = "https://carrentalapkstorage.blob.core.windows.net/images/corolla.jpg", IsAvailable = true },
-                new Car { Id = 2, CityId = 1, Brand = "Skoda", Model = "Octavia", Year = 2023, PricePerDay = 180.00m, ImageUrl = "https://carrentalapkstorage.blob.core.windows.net/images/octavia.jpg", IsAvailable = true },
-                new Car { Id = 3, CityId = 2, Brand = "Volkswagen", Model = "Golf", Year = 2021, PricePerDay = 130.00m, ImageUrl = "https://carrentalapkstorage.blob.core.windows.net/images/golf.jpg", IsAvailable = true },
-                new Car { Id = 4, CityId = 3, Brand = "BMW", Model = "3 Series", Year = 2024, PricePerDay = 250.00m, ImageUrl = "https://carrentalapkstorage.blob.core.windows.net/images/bmw3.jpg", IsAvailable = true }
+                new Car { Id = 1, CityId = 1, Brand = "Toyota", Model = "Corolla", Year = 2022, PricePerDay = 150.00m, ImageUrl = "https://carrentalapkstorage.blob.core.windows.net/obrazki/Corolla.jpg", IsAvailable = true },
+                new Car { Id = 2, CityId = 1, Brand = "Skoda", Model = "Octavia", Year = 2023, PricePerDay = 180.00m, ImageUrl = "https://carrentalapkstorage.blob.core.windows.net/obrazki/Octavia.jpg", IsAvailable = true },
+                new Car { Id = 3, CityId = 2, Brand = "Volkswagen", Model = "Golf", Year = 2021, PricePerDay = 130.00m, ImageUrl = "https://carrentalapkstorage.blob.core.windows.net/obrazki/Golf.jpg", IsAvailable = true },
+                new Car { Id = 4, CityId = 3, Brand = "BMW", Model = "3 Series", Year = 2024, PricePerDay = 250.00m, ImageUrl = "https://carrentalapkstorage.blob.core.windows.net/obrazki/bmw3.jpg", IsAvailable = true },
+                new Car { Id = 5, CityId = 4, Brand = "Fiat", Model = "500", Year = 2022, PricePerDay = 130.00m, ImageUrl = "https://carrentalapkstorage.blob.core.windows.net/obrazki/Fiat.jpg", IsAvailable = true },
+                new Car { Id = 6, CityId = 4, Brand = "Setra", Model = "Inter", Year = 2011, PricePerDay = 1500.00m, ImageUrl = "https://carrentalapkstorage.blob.core.windows.net/obrazki/autokar_mediolan.jpg", IsAvailable = true }
             );
         }
     }
---
+++ CarRentalAPI/CarRentalAPI/Migrations/20250628013400_Bloby.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CarRentalAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Bloby : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "https://carrentalapkstorage.blob.core.windows.net/obrazki/Corolla.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "https://carrentalapkstorage.blob.core.windows.net/obrazki/Octavia.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "https://carrentalapkstorage.blob.core.windows.net/obrazki/Golf.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "https://carrentalapkstorage.blob.core.windows.net/obrazki/bmw3.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "https://carrentalapkstorage.blob.core.windows.net/obrazki/warszawa.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "https://carrentalapkstorage.blob.core.windows.net/obrazki/krak%C3%B3w.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "https://carrentalapkstorage.blob.core.windows.net/obrazki/Gdansk.jpg");
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "ImageUrl", "Name" },
+                values: new object[] { 4, "https://carrentalapkstorage.blob.core.windows.net/obrazki/Mediolan.jpg", "Mediolan" });
+
+            migrationBuilder.InsertData(
+                table: "Cars",
+                columns: new[] { "Id", "Brand", "CityId", "ImageUrl", "IsAvailable", "Model", "PricePerDay", "Year" },
+                values: new object[,]
+                {
+                    { 5, "Fiat", 4, "https://carrentalapkstorage.blob.core.windows.net/obrazki/Fiat.jpg", true, "500", 130.00m, 2022 },
+                    { 6, "Setra", 4, "https://carrentalapkstorage.blob.core.windows.net/obrazki/autokar_mediolan.jpg", true, "Inter", 1500.00m, 2011 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "https://carrentalapkstorage.blob.core.windows.net/images/corolla.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "https://carrentalapkstorage.blob.core.windows.net/images/octavia.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "https://carrentalapkstorage.blob.core.windows.net/images/golf.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "https://carrentalapkstorage.blob.core.windows.net/images/bmw3.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "https://carrentalapkstorage.blob.core.windows.net/images/warsaw.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "https://carrentalapkstorage.blob.core.windows.net/images/krakow.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "https://carrentalapkstorage.blob.core.windows.net/images/gdansk.jpg");
+        }
+    }
+}
